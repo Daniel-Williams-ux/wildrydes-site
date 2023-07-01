7d4287b1997b0e78773c86ea2bd8f7cdeb2fece3
EC2 Default User <ec2-user@ip-172-31-30-110.ec2.internal>
2023-07-01 09:49:09 +0000

    new user pool client id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_wTpWrbndh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5h4rq71a7jk7qr2j750pshvmal', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
